cf21d456a43d8aa51d8086f704e650112cf2a6f7
jaarathy007 <www.arathikv007@gmail.com>
2025-01-30 05:26:34 +0000

    Add files via upload

---
+++ test.py
@@ -0,0 +1,9 @@
+from ops import *
+def test_add():
+    assert add(1,2) == 3
+def test_subtract():
+    assert subtract(5,2) == 2
+def test_multiply():
+    assert multiply(5,3) == 15
+def test_divide():
+    assert divide(10,2) == 5
