bfeb9194d74429f5827c224264aab0f905d319ca
jaarathy007 <www.arathikv007@gmail.com>
2025-02-12 05:19:04 +0000

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from ops import *
 def test_add():
     assert add(1,2) == 3
 def test_subtract():
-    assert subtract(5,2) == 2
+    assert subtract(5,2) == 3
 def test_multiply():
     assert multiply(5,3) == 15
 def test_divide():
